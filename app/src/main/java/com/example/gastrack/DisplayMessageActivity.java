8de7a897285779519c90d1420d89436ed692f077
patrick <neal.patrick@me.com>
2019-11-17 21:34:52 -0800

    Json Connected

--- app/src/main/java/com/example/gastrack/DisplayMessageActivity.java
+++ app/src/main/java/com/example/gastrack/DisplayMessageActivity.java
@@ -2,7 +2,9 @@ package com.example.gastrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        String jsonString = intent.getStringExtra("entryInfoJSON");
+
+        TextView textView = findViewById(R.id.textResultsCombined);
+        TextView textViewJSON = findViewById(R.id.textJSONObject);
+        textView.setText(message);
+        textViewJSON.setText(jsonString);
     }
 }
--- app/src/main/java/com/example/gastrack/MainActivity.java
+++ app/src/main/java/com/example/gastrack/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.gastrack;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.gastrack.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -25,4 +34,35 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    public void saveData (View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //Fields
+        EditText editLiters = (EditText) findViewById(R.id.editLiters);
+        EditText editPrice = (EditText) findViewById((R.id.editPrice));
+        EditText editPerLiter = (EditText) findViewById((R.id.editPerLiter));
+        //Strings
+        String editLitersString = editLiters.getText().toString();
+        String editPriceString = editPrice.getText().toString();
+        String editPerLiterString = editPerLiter.getText().toString();
+        //Combined
+        String message = "Liters: " + editLitersString + ", Price: " + editPriceString + ", Price per liter: " + editPerLiterString;
+        //create Json
+        JSONObject entryInfoJSON = createJson(editLitersString,editPriceString,editPerLiterString);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra("entryInfoJSON", entryInfoJSON.toString());
+        startActivity(intent);
+    }
+
+    public JSONObject createJson(String liters, String price, String perLiter ){
+        JSONObject entryInfoJSON = new JSONObject();
+        try {
+            entryInfoJSON.put("Liters", liters);
+            entryInfoJSON.put("Price", price);
+            entryInfoJSON.put("PerLiters", perLiter);
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+        return(entryInfoJSON);
+
+    }
 }
